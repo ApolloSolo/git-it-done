bf743a880aeeb0e85bc86be514f40979eaea2339
ApolloSolo <matt1teixeira@gmail.com>
2022-02-20 19:04:15 -0500

    Link to view more than the the limited 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,13 +1,19 @@
 const issueContainerEl = document.querySelector("#issues-container");
+const limitWarningEl = document.querySelector("#limit-warning");
 
 const getRepoIssues = function(repo){
-    const apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    const apiUrl = "https://api.github.com/repos/facebook/react/issues?direction=asc";
     fetch(apiUrl)
     .then((res) => {
         if(res.ok){
             res.json().then((data) => {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (res.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             })
         }
         else{
@@ -20,6 +26,11 @@ const getRepoIssues = function(repo){
 }
 
 const displayIssues = function(issues) {
+
+    if(issues.length === 0){
+        issueContainerEl.textContent = "This repo has no open issues!";
+    }
+
     for (let i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         const issueEl = document.createElement("a");
@@ -51,5 +62,18 @@ const displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
       }
 };
+
+const displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    const linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 getRepoIssues("ApolloSolo/frac-tank-db");
